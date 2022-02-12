e96eaf4f9bb1309513bfe1547fd6e1769170e896
prafull <prafull@thinkjs.club>
2022-02-12 21:36:18 +0530

    args in component level and story level

--- src/Components/Button/Button.stories.js
+++ src/Components/Button/Button.stories.js
@@ -4,9 +4,37 @@ import Button from "./index";
 export default {
   title: "Form/Button",
   component: Button,
+  args: {
+    children: "Button", // Args at component level remember:Args at story level overwrite on component level
+  },
 };
 
-export const Primary = () => <Button variant="primary">Primary</Button>;
-export const Secondary = () => <Button variant="secondary">Secondary</Button>;
-export const Success = () => <Button variant="success">Success</Button>;
-export const Danger = () => <Button variant="danger">Danger</Button>;
+// for understanding
+// export const Primary = () => <Button variant="primary">Primary</Button>;
+// export const Secondary = () => <Button variant="secondary">Secondary</Button>;
+// export const Success = () => <Button variant="success">Success</Button>;
+// export const Danger = () => <Button variant="danger">Danger</Button>;
+
+//Used Approach
+const Template = (args) => <Button {...args} />;
+
+export const PrimaryA = Template.bind({});
+
+PrimaryA.args = {
+  variant: "primary", // args at story level
+  children: "primary args",
+};
+
+export const SecondaryA = Template.bind({});
+
+SecondaryA.args = {
+  variant: "secondary",
+  children: "secondary args",
+};
+
+export const LongPrimaryArgs = Template.bind({});
+
+LongPrimaryArgs.args = {
+  ...PrimaryA.args,
+  children: "Long Primary Argss",
+};
